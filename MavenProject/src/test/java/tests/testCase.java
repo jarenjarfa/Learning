46d0d8bd6d2cedb7e2afc0222c84012db42615b5
jarfa <jarfa@jarfa>
2019-11-26 22:52:09 +0700

    add test webelements

--- MavenProject/src/test/java/tests/testCase.java
+++ MavenProject/src/test/java/tests/testCase.java
@@ -1,7 +1,11 @@
 package tests;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class testCase {
 	
@@ -15,5 +19,11 @@ public class testCase {
 
 		// Click on Check Button
 		driver.findElement(By.id("buttoncheck")).click();
+		List<WebElement> elements = new ArrayList<>();
+		elements = driver.findElements(By.name("name"));
+	    System.out.println("Number of elements:" +elements.size());
+		for (int i=0; i<elements.size();i++){
+		      System.out.println("Radio button text:" + elements.get(i).getAttribute("value"));
+		}
 	}
 }
